90949a1763d2d7099f5826f6a4c5972a65873ad2
RaresDima <rares.dima96@yahoo.com>
2024-12-04 18:17:40 +0200

    main user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,9 +3,11 @@ part of 'models.dart';
 class User {
   final int? id;
   final String name;
+  final bool isMainUser;
 
   const User({ 
     this.id, 
-    required this.name 
+    required this.name, 
+    required this.isMainUser
   });
 }
