6a4148c1ba1c2fe5f2fe43d1d32e9e3b46bd494e
RaresDima <rares.dima96@yahoo.com>
2024-12-05 00:50:11 +0200

    weekend colors

--- lib/view/calendar_page.dart
+++ lib/view/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -15,6 +16,7 @@ class _CalendarPageState extends State<CalendarPage> {
 
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
 
   void _incrementCounter() {
@@ -32,7 +34,7 @@ class _CalendarPageState extends State<CalendarPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             TableCalendar(
               firstDay: DateTime.utc(1950, 10, 1),  // Month must be 10 or the calendar gets fucked up...
@@ -42,11 +44,63 @@ class _CalendarPageState extends State<CalendarPage> {
                 return isSameDay(_selectedDay, day);
               },
               onDaySelected: (selectedDay, focusedDay) {  // On day tapped
+                print(focusedDay);
                 setState(() {
                   _selectedDay = selectedDay;  // focusedDay will go to 30/31 of cur mth if day of next mth tapped
-                  _focusedDay = _focusedDay;   // this is so it doesnt scroll to next mth
+                  _focusedDay = focusedDay;    // this is so it doesnt scroll to next mth
                 });
               },
+              calendarFormat: _calendarFormat,  // Show 1 month of days, 2 weeks or 1 week
+              onFormatChanged: (format) {       // On tap format changes to next value
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              calendarBuilders: CalendarBuilders(
+                dowBuilder: (context, day) {  // Day of week label
+                  final String text = DateFormat.E().format(day);
+                  TextStyle textStyle = TextStyle(color: Colors.black);
+                  if (day.weekday == DateTime.saturday || day.weekday == DateTime.sunday) {
+                    textStyle = TextStyle(color: Colors.red);
+                  }
+                  return Center(
+                    child: Text(
+                      text,
+                      style: textStyle,
+                    ),
+                  );
+                },
+                defaultBuilder: (context, day, focusedDay) {  // Regular non-special days in the current month
+                  final String text = day.day.toString();
+                  TextStyle textStyle = TextStyle(color: Colors.black);
+                  if (day.weekday == DateTime.saturday || day.weekday == DateTime.sunday) {
+                    textStyle = TextStyle(color: Colors.red);
+                  }
+                  return Center(
+                    child: Text(
+                      text,
+                      style: textStyle,
+                    ),
+                  );
+                },
+                outsideBuilder: (context, day, focusedDay) {  // Regular non-special days in the current month
+                  final String text = day.day.toString();
+                  TextStyle textStyle = TextStyle(color: Colors.grey);
+                  if (day.weekday == DateTime.saturday || day.weekday == DateTime.sunday) {
+                    textStyle = TextStyle(color: Colors.red.shade200);
+                  }
+                  return Center(
+                    child: Text(
+                      text,
+                      style: textStyle,
+                    ),
+                  );
+                },
+                // headerTitleBuilder: (context, day) { return Text('a'); }  // Month + Year header
+              ),
             ),
             Text('You have pushed the button this many times:'),
             Text(
